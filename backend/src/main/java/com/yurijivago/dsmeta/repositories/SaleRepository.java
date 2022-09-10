b75603bb897e799ea863c1d2fa3031218f14b483
Yuri Jivago <yurijivagoads@gmail.com>
2022-09-10 14:51:06 -0300

    API test

---
+++ backend/src/main/java/com/yurijivago/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,7 @@
+package com.yurijivago.dsmeta.repositories;
+
+import com.yurijivago.dsmeta.entities.Sale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+}
---
+++ backend/src/main/java/com/yurijivago/dsmeta/services/SaleService.java
@@ -0,0 +1,19 @@
+package com.yurijivago.dsmeta.services;
+
+import com.yurijivago.dsmeta.entities.Sale;
+import com.yurijivago.dsmeta.repositories.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository repository;
+
+    public List<Sale> findSales(){
+        return repository.findAll();
+    }
+}
